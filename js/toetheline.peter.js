3a560e086d70cd36002b94780a42790f5a3fa5fa
Peter Cook <peterrcook@yahoo.co.uk>
2012-11-24 20:09:41 +0000

    Some initial markup+styling for mp positioning

--- js/toetheline.peter.js
+++ js/toetheline.peter.js
@@ -2,9 +2,17 @@
 
 jQuery(function() {
     var mpTemplate = jQuery('script[type="text/tim"].mp').text();
-    var data = {name: 'Crispin Blunt', avatar: 'http://www.theyworkforyou.com/images/mps/10051.jpg'};
 
-    var html = tim(mpTemplate, data);
 
-    jQuery('div.rebels').append(html);
+    var data = [{name: 'Crispin Blunt', avatar: 'http://www.theyworkforyou.com/images/mps/10051.jpg'},
+				{name: 'Diane Abbott', avatar: 'http://www.theyworkforyou.com/images/mpsL/10001.jpeg'}];
+
+	for(var i=0, len=data.length; i<len; i++) {
+		var d = data[i];
+		d.position = i;
+	    var html = tim(mpTemplate, d);
+	    jQuery('div.rebels').append(html);
+	}
+
+
 });
