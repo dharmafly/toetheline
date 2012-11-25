fc128e4ea634dd3018aee66f84f767d3d48c1b07
Premasagar Rose <p@dharmafly.com>
2012-11-25 15:06:47 +0000

    Add rebel data

--- js/toetheline.js
+++ js/toetheline.js
@@ -2,37 +2,20 @@
 
 var PUBLICWHIP_BASEURL = 'http://www.publicwhip.org.uk/',
 	PUBLICWHIP_POLICIES = PUBLICWHIP_BASEURL + 'policies.php',
+	PUBLICWHIP_REBELS = PUBLICWHIP_BASEURL + 'mps.php?sort=rebellions',
 	PUBLICWHIP_POLICY_ID_REGEXP = /\d+$/,
 	PUBLICWHIP_BILL_ID_REGEXP = /number=(\d+)\b/,
-	PUBLICWHIP_MP_ID_REGEXP = /mpn=(\w+)\b/;
+	PUBLICWHIP_MP_ID_REGEXP = /mpn=(\w+)\b/,
+	THEYWORKFORYOU_BASEURL = 'http://www.theyworkforyou.com';
 
-/*
-function mp(id){
-	this.id = id;
-	this.bills = [];
-	this.policies = [{
-		policy: REF,
-		rebelled: 0,
-		abstained: 0,
-		voted: 0
-	}];
-	this.totals = {
-		rebelled: 0,
-		abstained: 0,
-		voted: 0
-	};
-}
-mp.prototype = {
-};
-*/
+function Policy(settings){
+	jQuery.extend(this, settings);
+	this.title = jQuery.trim(this.title);
+	this.url = this.setUrl(settings.path);
+	this.id = this.setId(settings.path);
 
-function Policy(title, path){
-	this.title = title,
-	this.url = this.setUrl(path);
-	this.id = this.setId(path);
-
-	if (!title || !path || this.id === null){
-		throw 'Policy error: ' + this.id + ' | ' + path + ' | ' + title;
+	if (!this.title || !this.path || this.id === null){
+		throw 'Policy error: ' + this.id + ' | ' + this.path + ' | ' + this.title;
 	}
 }
 Policy.prototype = {
@@ -45,13 +28,14 @@ Policy.prototype = {
 	}
 }
 
-function Bill(title, path){
-	this.title = title,
-	this.url = this.setUrl(path);
-	this.id = this.setId(path);
+function Bill(settings){
+	jQuery.extend(this, settings);
+	this.title = jQuery.trim(settings.title);
+	this.url = this.setUrl(settings.path);
+	this.id = this.setId(settings.path);
 
-	if (!title || !path || this.id === null){
-		throw 'Bill error: ' + this.id + ' | ' + path + ' | ' + title;
+	if (!this.title || !this.path || this.id === null){
+		throw 'Bill error: ' + this.id + ' | ' + this.path + ' | ' + this.title;
 	}
 }
 Bill.prototype = {
@@ -64,13 +48,14 @@ Bill.prototype = {
 	}
 }
 
-function MP(name, path){
-	this.name = name,
-	this.url = this.setUrl(path);
-	this.id = this.setId(path);
+function MP(settings){
+	jQuery.extend(this, settings);
+	this.title = jQuery.trim(settings.title);
+	this.url = this.setUrl(settings.path);
+	this.id = this.setId(settings.path);
 
-	if (!name || !path || this.id === null){
-		throw 'MP error: ' + this.id + ' | ' + path + ' | ' + name;
+	if (!this.title || !this.path || this.id === null){
+		throw 'MP error: ' + this.id + ' | ' + this.path + ' | ' + this.title;
 	}
 }
 MP.prototype = {
@@ -80,9 +65,6 @@ MP.prototype = {
 	setId: function(path){
 		var matches = path.match(PUBLICWHIP_MP_ID_REGEXP);
 		return matches ? matches[1] : null;
-	},
-	setName: function(){
-		return this.id.replace('')
 	}
 }
 
@@ -91,11 +73,24 @@ function dataProcessor(Constructor){
 		var collection = {};
 
 		_.each(response[0].results, function(data){
-			var item = new Constructor(data.text, data.href);
+			var item = new Constructor({
+				title: data.text,
+				path: data.href
+			});
 			collection[item.id] = item;
 		});
 		return collection;
 	}
+}
+
+// Current members
+function getRebels(url){
+	return noodle.get({
+		url:  PUBLICWHIP_REBELS,
+		type: 'html',
+		selector: 'table.mps tr:not(.headings)',
+		extract: ['html']
+	});
 }
 
 function getPolicies(url){
@@ -123,6 +118,42 @@ function getMPsFromBill(url){
 		selector: '#votetable tr td:first-child a',
 		extract: ['href', 'text']
 	});
+}
+
+function getMPTheyWorkForYouUrl(url){
+	return noodle.get({
+		url:  url,
+		type: 'html',
+		selector: '#main li a[href^="http://www.theyworkforyou.com/"]',
+		extract: ['href']
+	});
+}
+
+function getMPTheyWorkForYouMP(url){
+	return noodle.get({
+		url:  url,
+		type: 'html',
+		selector: '.main',
+		extract: ['html', 'src']
+	});
+}
+
+function processRebels(response){
+	var rebels = {};
+
+	_.each(response[0].results, function(data){
+		var cells = jQuery(data.html),
+			mp = new MP({
+				title: cells.eq(0).text(),
+				path: cells.eq(0).find('a').attr('href'),
+				consituency: cells.eq(1).text(),
+				consituencyUrl: cells.eq(1).attr('href'),
+				party: cells.eq(2).text(),
+				rebelliousness: parseInt(cells.eq(3).text())
+			});
+			rebels[mp.id] = mp;
+	});
+	return rebels;
 }
 
 function fetchBillsFromPolicies(policies){
@@ -172,27 +203,100 @@ function fetchMPsFromBills(policies){
 		});
 }
 
+function fetchMPsExtended(mps){
+	var promises = [];
+
+	_.each(mps, function(mp){
+		var mpDeferred = jQuery.Deferred();
+		promises.push(mpDeferred.promise());
+
+		getMPTheyWorkForYouUrl(mp.url)
+			.pipe(function(response){
+				mp.theyworkforyouUrl = response[0].results[0].href;
+
+				getMPTheyWorkForYouMP(mp.theyworkforyouUrl)
+					.pipe(function(response){
+						var data = response[0].results[0],
+							dom = jQuery(data.html),
+							avatar = dom.filter('.person').find('img').attr('src'),
+							//h1 = dom.find('h1'),
+							//title = h1[0].childNodes[0],
+							position = dom.filter('h1').find('span')[0],
+							party = position.childNodes[0].textContent,
+							consituency = position.childNodes[2] ? position.childNodes[2].textContent.replace(' for ', '') : position.childNodes[0].textContent;
+
+						jQuery.extend(mp, {
+							avatar: THEYWORKFORYOU_BASEURL + avatar,
+							party: party,
+							consituency: consituency
+						});
+
+						mpDeferred.resolve(mp);
+					});
+			});
+	});
+
+	return jQuery.when.apply(jQuery, promises)
+		.pipe(function(){
+			return mps;
+		});
+}
+
 function init(){
-	var policiesPromise, mpsPromise, policies, mps;
+	var rebelsPromise, policiesPromise, mpsPromise;
+
+	rebelsPromise = getRebels(PUBLICWHIP_REBELS)
+		.pipe(processRebels);
 
 	policiesPromise = getPolicies(PUBLICWHIP_POLICIES)
 		.pipe(dataProcessor(Policy))
-		.pipe(fetchBillsFromPolicies)
-		.pipe(function(data){
-			policies = data;
-			return data;
-		});
+		.pipe(fetchBillsFromPolicies);
 
 	mpsPromise = policiesPromise
 		.pipe(fetchMPsFromBills)
-		.pipe(function(data){
-			mps = data;
-			return data;
-		});
+		.pipe(fetchMPsExtended);
 
-	jQuery.when(policiesPromise, mpsPromise)
-		.then(function(){
-			var data = {
+	jQuery.when(rebelsPromise, policiesPromise, mpsPromise)
+		.then(function(rebels, policies, mps){
+			var data;
+
+			// Add overall rebelliousness
+			_.each(rebels, function(rebel){
+				// TODO: why are some rebels not in mps collection?
+				if (mps[rebel.id]){
+					mps[rebel.id].rebelliousness = rebel.rebelliousness;
+				}
+			});
+			
+			_.each(policies, function(policy){
+				policy.rebels = {};
+
+				_.each(policy.bills, function(bill){
+					_.each(bill.rebels, function(mpId){
+						var mp = mps[mpId];
+						if (!policy.rebels[mpId]){
+							policy.rebels[mpId] = 1;
+						}
+						else {
+							policy.rebels[mpId] ++;
+						}
+					});
+				});
+
+				policy.rebels = _.map(policy.rebels, function(rebelliousness, mpId){
+					return {
+						mp: mpId,
+						rebelliousness: rebelliousness
+					};
+				});
+
+				policy.rebels = _.sortBy(policy.rebels, function(mp){
+					return 100 - mp.rebelliousness;
+				});
+			});
+
+
+			data = {
 				mps: mps,
 				policies: policies
 			};
@@ -202,10 +306,13 @@ function init(){
 }
 
 function render(data){
+	window.data = data;
 	console.log(data);
 }
 
 /////
 
 // Go!
-init();
+if (window.location.search === '?live'){
+	init();
+}
